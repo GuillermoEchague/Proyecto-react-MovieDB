8f5d71311b7626267eddfabc704bda6a0383b430
Guillermo Echague <guillermo.echaguea@gmail.com>
2021-06-08 20:41:56 -0400

    Details works!

--- src/components/CharacterDetail.jsx
+++ src/components/CharacterDetail.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "../assets/styles/components/CharacterDetail.scss"
+import {BASE_PATH_IMG} from '../utils/constants';
 
-const MovieDetail = () => (
-    <p> Hello I am MovieDetail </p>
-)
-export default MovieDetail;
+const CharacterDetail = (props) => {
+    const { data } = props;
+    const {title,overview, backdrop_path } = data;
+
+    return(
+        <div className="characterDetail">
+            <img className="characterDetail-img" src={`${BASE_PATH_IMG}${backdrop_path}`} alt="detail movie"/>
+            <div className="characterDetail__container">
+                <h2 className="characterDetail-name">{title}</h2>
+                <p className="characterDetail__item">
+                    <span className="characterDetail__item-type">Información general:</span>
+                    {' '}
+                    {overview}
+                </p>
+            </div>
+        
+        </div>
+    )
+}
+
+export default CharacterDetail;
